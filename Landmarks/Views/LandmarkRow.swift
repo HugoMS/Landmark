7077117f2a72844c6c6d4339c3a6471846b99496
Hugo Morelli <hugomorelli2@gmail.com>
2021-02-26 16:22:30 +0100

    [feat] add isFavorite

--- Landmarks/Views/LandmarkRow.swift
+++ Landmarks/Views/LandmarkRow.swift
@@ -17,6 +17,10 @@ struct LandmarkRow: View {
                 .frame(width: 50, height: 50)
             Text(landmark.name)
             Spacer()
+            if landmark.isFavorite {
+                Image(systemName: "star.fill")
+                    .foregroundColor(.yellow)
+            }
         }
     }
 }
@@ -27,6 +31,6 @@ struct LandmarkRow_Previews: PreviewProvider {
             LandmarkRow(landmark: landmarks[0])
             LandmarkRow(landmark: landmarks[1])
         }
-        .previewLayout(.fixed(width: 300, height: 70))
+            .previewLayout(.fixed(width: 300, height: 70))
     }
 }
