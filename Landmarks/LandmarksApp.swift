e891c38f28bf32d54b51246268fb71a67206ebc9
Hugo Morelli <hugomorelli2@gmail.com>
2021-02-26 08:17:57 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Hugo Morelli on 26/2/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
