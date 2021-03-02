ae95d01578459d9d0bb0b2dad4096e2ca70015c1
Hugo Morelli <hugomorelli2@gmail.com>
2021-03-02 08:09:28 +0100

    [feat] init Add a Category View

---
+++ Landmarks/Views/Category/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Hugo Morelli on 2/3/21.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text("Hello, World!")
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
